4276002231bb952afc34295194a58d4696eec90c
Bartolem <97914472+Bartolem@users.noreply.github.com>
2022-05-26 22:45:32 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,159 @@
+input = require('sync-input');
+
+const camel =`
+Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \\
+     |     \\    _.-'             \\
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;     
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!`;
+
+const lion = `
+Switching on the camera in the lion habitat...
+                                               ,w.
+                                             ,YWMMw  ,M  ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; \`,       /\`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+,MM:.    .'.-'   .'     ;     \`\\    ;     \`,     MMMMMMMW \`"=./\`-,
+WMMm__,-'.'     /      _.\\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / \`=_}
+"^MP__.-'    ,-' _.--""   \`-,   ;       \\  ; ;MMMMMMMMMMW^\`\`; __|
+           /   .'            ; ;         )  )\`{  \\ \`"^W^\`,   \\  :
+          /  .'             /  (       .'  /     Ww._     \`.  \`"
+         /  Y,              \`,  \`-,=,_{   ;      MMMP\`""-,  \`-._.-,
+        (--, )                \`,_ / \`) \\/"")      ^"      \`-, -;"\\:
+The lion is roaring!`;
+
+const deer = `
+Switching on the camera in the deer habitat...
+   /|       |\\
+\`__\\\\       //__'
+   ||      ||
+ \\__\`\\     |'__/
+   \`_\\\\   //_'
+   _.,:---;,._
+   \\_:     :_/
+     |@. .@|
+     |     |
+     ,\\.-./ \\
+     ;;\`-'   \`---__________-----.-.
+     ;;;                         \\_\\
+     ';;;                         |
+      ;    |                      ;
+       \\   \\     \\        |      /
+        \\_, \\    /        \\     |\\
+          |';|  |,,,,,,,,/ \\    \\ \\_
+          |  |  |           \\   /   |
+          \\  \\  |           |  / \\  |
+           | || |           | |   | |
+           | || |           | |   | |
+           | || |           | |   | |
+           |_||_|           |_|   |_|
+          /_//_/           /_/   /_/
+Our 'Bambi' looks hungry. Let's go to feed it!`;
+
+const goose = `
+Switching on the camera in the goose habitat...
+
+                                    _
+                                ,-"" "".
+                              ,'  ____  \`.
+                            ,'  ,'    \`.  \`._
+   (\`.         _..--.._   ,'  ,'        \\    \\
+  (\`-.\\    .-""        ""'   /          (  d _b
+ (\`._  \`-"" ,._             (            \`-(   \\
+ <_  \`     (  <\`<            \\              \`-._\\
+  <\`-       (__< <           :
+   (__        (_<_<          ;
+    \`------------------------------------------
+The goose is staring intently at you... Maybe it's time to change the channel?`;
+
+const bat = `
+Switching on the camera in the bat habitat...
+_________________               _________________
+ ~-.              \\  |\\___/|  /              .-~
+     ~-.           \\ / o o \\ /           .-~
+        >           \\\\  W  //           <
+       /             /~---~\\             \\
+      /_            |       |            _\\
+         ~-.        |       |        .-~
+            ;        \\     /        i
+           /___      /\\   /\\      ___\\
+                ~-. /  \\_/  \\ .-~
+                   V         V
+This bat looks like it’s doing fine.`;
+
+const rabbit = `
+Switching on the camera in the rabbit habitat...
+         ,
+        /|      __
+       / |   ,-~ /
+      Y :|  //  /
+      | jj /( .^
+      >-"~"-v"
+     /       Y
+    jo  o    |
+   ( ~T~     j
+    >._-' _./
+   /   "~"  |
+  Y     _,  |
+ /| ;-"~ _  l
+/ l/ ,-"~    \\
+\\//\\/      .- \\
+ Y        /    Y
+ l       I     !
+ ]\\      _\\    /"\\
+(" ~----( ~   Y.  )
+It looks like we will soon have more rabbits!`;
+
+const habitat = [camel, lion, deer, goose, bat, rabbit ];
+
+let prom = input("Which habitat # do you need? ");
+
+while (true) {
+    switch (prom) {
+        case "0":
+            console.log(habitat[0]);
+            prom = input("Which habitat # do you need?");
+            continue;
+        case "1":
+            console.log(habitat[1]);
+            prom = input("Which habitat # do you need?");
+            continue;
+        case "2":
+            console.log(habitat[2]);
+            prom = input("Which habitat # do you need?");
+            continue;
+        case "3":
+            console.log(habitat[3]);
+            prom = input("Which habitat # do you need?");
+            continue;
+        case "4":
+            console.log(habitat[4]);
+            prom = input("Which habitat # do you need?");
+            continue;
+        case "5":
+            console.log(habitat[5]);
+            prom = input("Which habitat # do you need?");
+            continue;
+        case "exit":
+            console.log("See you later!");
+            break;
+    }
+    break;
+}
